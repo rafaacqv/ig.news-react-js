28377804f4c1cc59d864c3123e50e22e7384dbb6
Rafael Araujo <rafaacq@gmail.com>
2021-04-15 18:03:46 -0300

    Fix dynamic title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
+import Head from 'next/head';
+
 import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello World!</h1>
+    <h1 className={styles.title}>
+        <Head>
+          <title>Início | ig.news</title>
+        </Head>
+
+      Hello World!
+    </h1>
   )
 }
