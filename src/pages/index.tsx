aa2c6a389f8c3b485f5c47293f0a25b86daa34ba
Rafael Araujo <rafaacq@gmail.com>
2021-04-19 16:20:44 -0300

    Title adjustments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,14 @@ import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-        <Head>
-          <title>Início | ig.news</title>
-        </Head>
-
-      Hello World!
-    </h1>
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      
+      <h1 className={styles.title}>
+        Hello World!
+      </h1>
+    </>
   )
 }
